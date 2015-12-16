410d745d5021f73d9e502bc71cb2df28bf76d11f
iakud <kaikai87111@gmail.com>
2015-12-16 16:52:58 +0800

    fix

--- example/example.cpp
+++ example/example.cpp
@@ -5,18 +5,19 @@ int main() {
 	exampleTable tb;
 	const exampleTable::Set& items = tb.getItems();
 	for (exampleTable::Set::const_iterator it = items.begin(); it != items.end(); ++it) {
-		std::cout << "int16 Value:" << (*it)->i16 << std::endl;
-		std::cout << "int32 Value:" << (*it)->i32 << std::endl;
-		std::cout << "int64 Value:" << (*it)->i64 << std::endl;
-		printf("float, double: %f, %f\n", (*it)->f, (*it)->df);
-		std::cout << "string Value:" << (*it)->s << std::endl;
+		std::cout << "int16 Value: " << (*it)->i16 << std::endl;
+		std::cout << "int32 Value: " << (*it)->i32 << std::endl;
+		std::cout << "int64 Value: " << (*it)->i64 << std::endl;
+		printf("float Value: %f\n", (*it)->f);
+		printf("double Value: %f\n", (*it)->df);
+		std::cout << "string Value :" << (*it)->s << std::endl;
 		int size = static_cast<int>((*it)->v.size());
 		for (int i = 0; i < size; ++i) {
-			std::cout << "vector Value:" << (*it)->v[i] << std::endl;
+			std::cout << "vector Value :" << (*it)->v[i] << std::endl;
 		}
 		std::map<std::string,std::string>::iterator itm = (*it)->m.begin();
 		for (; itm!=(*it)->m.end(); ++itm) {
-			std::cout << "map Value:" << itm->first << "," << itm->second << std::endl;
+			std::cout << "map Value :" << itm->first << ", " << itm->second << std::endl;
 		}
 	}
 	return 0;
