427ae22e73a82190e5c551ab160960d73ece36f5
iakud <kaikai87111@gmail.com>
2015-12-16 16:42:54 +0800

    example

--- example/example.cpp
+++ example/example.cpp
@@ -2,21 +2,21 @@
 #include <iostream>
 
 int main() {
-	ExampleTable tb;
-	const ExampleTable::Set& items = tb.getItems();
-	for (ExampleTable::Set::const_iterator it = items.begin(); it != items.end(); ++it) {
-		std::cout << "int16Value:" << (*it)->int16Value << std::endl;
-		std::cout << "int32Value:" << (*it)->int32Value << std::endl;
-		std::cout << "int64Value:" << (*it)->int64Value << std::endl;
-		printf("%f,%f", (*it)->floatValue, (*it)->doubleValue);
-		std::cout << "stringValue:" << (*it)->stringValue << std::endl;
+	exampleTable tb;
+	const exampleTable::Set& items = tb.getItems();
+	for (exampleTable::Set::const_iterator it = items.begin(); it != items.end(); ++it) {
+		std::cout << "int16 Value:" << (*it)->i16 << std::endl;
+		std::cout << "int32 Value:" << (*it)->i32 << std::endl;
+		std::cout << "int64 Value:" << (*it)->i64 << std::endl;
+		printf("float, double: %f, %f", (*it)->f, (*it)->df);
+		std::cout << "string Value:" << (*it)->s << std::endl;
 		int size = (*it)->vectorValue.size();
 		for (int i = 0; i < size; ++i) {
-			std::cout << "vectorValue:" << (*it)->vectorValue[i] << std::endl;
+			std::cout << "vector Value:" << (*it)->v[i] << std::endl;
 		}
-		std::map<std::string,std::string>::iterator itmap = (*it)->mapValue.begin();
-		for (; itmap!=(*it)->mapValue.end(); ++itmap) {
-			std::cout << "mapValue:" << itmap->first << "," << itmap->second << std::endl;
+		std::map<std::string,std::string>::iterator itm = (*it)->m.begin();
+		for (; itm!=(*it)->m.end(); ++itm) {
+			std::cout << "map Value:" << itm->first << "," << itm->second << std::endl;
 		}
 	}
 	return 0;
